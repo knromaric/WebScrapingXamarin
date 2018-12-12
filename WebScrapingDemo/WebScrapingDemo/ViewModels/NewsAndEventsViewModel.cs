67eed62b136b39a0ebf2dd56c14487c31216e398
knromaric <nzekeng@gmail.com>
2018-12-12 13:33:30 +0200

    Added NewsAndEvents Page implementation.

---
+++ WebScrapingDemo/WebScrapingDemo/ViewModels/NewsAndEventsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebScrapingDemo.Models;
+
+namespace WebScrapingDemo.ViewModels
+{
+    public class NewsAndEventsViewModel: BaseViewModel
+    {
+        public ObservableCollection<NewsAndEvents> NewsAndEventsCollection { get; set; }
+
+        public NewsAndEventsViewModel()
+        {
+            NewsAndEventsCollection = new ObservableCollection<NewsAndEvents>();
+        }
+
+        public async Task SetData()
+        {
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(@"http://piranigroup.com.pk/news-events/");
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var nodes = htmlDocument.DocumentNode
+                                    .Descendants("div")
+                                    .Where(n => n.GetAttributeValue("class", "").Equals("rbs-img-image  rbs-lightbox"));
+
+            foreach (var node in nodes)
+            {
+                var newsAndEvent = new NewsAndEvents
+                {
+                    ThumbNail = node.Descendants("div").FirstOrDefault()?.ChildAttributes("data-thumbnail").FirstOrDefault()?.Value
+                };
+
+                NewsAndEventsCollection.Add(newsAndEvent);
+            }
+        }
+    }
+}
--- WebScrapingDemo/WebScrapingDemo/Views/NewsAndEventsPage.xaml.cs
+++ WebScrapingDemo/WebScrapingDemo/Views/NewsAndEventsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WebScrapingDemo.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,19 @@ namespace WebScrapingDemo.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NewsAndEventsPage : ContentPage
 	{
-		public NewsAndEventsPage ()
+        private NewsAndEventsViewModel _newsAndEventsViewModel;
+        public NewsAndEventsPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            _newsAndEventsViewModel = new NewsAndEventsViewModel();
+            BindingContext = _newsAndEventsViewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await _newsAndEventsViewModel.SetData();
+        }
+    }
 }
